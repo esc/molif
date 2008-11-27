6bd2f0ecd55d80e5637d7bb859d7ca06daf93aed
valentin <valentin@024aea0a-228c-ee1e-b6e1-9d36f26932e4>
2008-11-27 18:48:40 +0000

    Updated the make file to produce a source distribution
    Modified the parameters of the LIF model to those suggested by bartosz
    in the paper.
    
    Refactored the lnlif model into a class that is passed to the euler
    solver.
    
    
    
    git-svn-id: svn+ssh://bolero.cs.tu-berlin.de/home/valentin/svn/curiolab@2 024aea0a-228c-ee1e-b6e1-9d36f26932e4

--- software/l-nlif.py
+++ software/l-nlif.py
@@ -1,56 +1,68 @@
 from pylab import *
 from scipy import *
 
-t_max = 10000
-
-# leak reversal potential : volt
-V_leak = -10
-# membrane leak conductance
-g = 0.01
-# reset voltage, should be < 1
-V_reset = -40
-# threshold voltage
-V_threshold = 15
-
-# model of the stimulus as a vector
-# TODO this should be fixed
-stim = rand(t_max)
-#noise = rand(t_max)
-
-# spationtemporal linear kernel
-# in this case modeled as a difference of gaussians
-n = stats.distributions.norm
-x = arange(-10,10,0.2)
-pos = stats.distributions.norm_gen.pdf(n,x,loc=0)
-neg = stats.distributions.norm_gen.pdf(n,x,loc=-5,scale=1.5)
-k =  0.2 *( pos - neg)
-
-# now convolve the linear filter k with the stimulus
-# this really should be of length t_max
-# check that the convolution does indeed return this.
-# may need to pad out the linear kernel
-
-I_stim = convolve(stim,k, mode='same')
+# TODO:
+# Add noise, at the moment it will have the same response
+# the noise should be gaussian.
+# what to do about the stimulus what should it be
+# what would be a linear filter, a.k.a. kernel
+# what about the I_hist, i don't even know what this should look like.
 
 
-def lnlif(x,t):
-    ''' linear - noisy leaky integrate and fire model '''
-    if x >= V_threshold:
-        return V_reset
-    else:
-        return -g*(x - V_leak) + I_stim[t] #+ I_hist(t)
-    
-def I_hist(t):
-    # in the case where the temporal basis functions are not knowen
-    # returning zero here brings us back to the more standard LIF
-    # model
-    # TODO this needs to be implemented
-    return 0
+class lnlif:
+
+    t_max = 10000
+
+    # leak reversal potential : volt
+    V_leak = -80
+    # membrane leak conductance
+    g = 0.00001
+    # reset voltage, should be < 1
+    V_reset = -80
+    # threshold voltage
+    V_threshold = -54
+
+    # model of the stimulus as a vector
+    # TODO this should be fixed
+    stim = rand(t_max)
+    #noise = rand(t_max)
+
+    # spationtemporal linear kernel
+    # in this case modeled as a difference of gaussians
+    n = stats.distributions.norm
+    x = arange(-10,10,0.05)
+    pos = stats.distributions.norm_gen.pdf(n,x,loc=0)
+    neg = stats.distributions.norm_gen.pdf(n,x,loc=-4,scale=2)
+    k = 0.7 * ( pos - neg)
+
+    # now convolve the linear filter k with the stimulus
+    # this really should be of length t_max
+    # check that the convolution does indeed return this.
+    # may need to pad out the linear kernel
+
+    I_stim = 0.2 + convolve(stim,k, mode='same')
+    spikes = []
 
 
-def euler(f_func, x_0, t_max, dt):
+    def integrate(self,x,t):
+        if x >= self.V_threshold:
+            self.spikes.append(t)
+            return self.V_reset
+        else:
+            return -self.g*(x - self.V_leak) + self.I_stim[t] #+ I_hist(t)
+        
+  
+    def I_hist(self,t):
+        # in the case where the temporal basis functions are not knowen
+        # returning zero here brings us back to the more standard LIF
+        # model
+        # TODO this needs to be implemented
+        return 0
+
+
+def euler(lif, x_0, t_max, dt):
     """ euler method for for solving ODEs
-    f_func - a method pointer
+    lif - a class that implements the integrate(x,t) function
     x_0 initial value for x
     t_max maximum time
     dt change in time """
@@ -58,17 +70,21 @@ def euler(f_func, x_0, t_max, dt):
     result[0] = x_0 
     time = arange(0,t_max,dt)
     for i in xrange (1,int(t_max/dt)):
-        result[i] = result[i-1] + f_func(result[i-1],time[i-1]) * dt #+ noise[i]
+        result[i] = result[i-1] + lif.integrate(result[i-1],time[i-1]) * dt #+ noise[i]
     return (time, result)
 
+lif = lnlif()
 
-time, result = euler(lnlif,-40,t_max,1)
+time, result = euler(lif,-80,lif.t_max,1)
+
+print lif.spikes
 
 subplot(2,2,1), plot(time,result), title('output')
-subplot(2,2,2), plot(k), title('k')
-subplot(2,2,3), plot(time,I_stim), title('I_stim')
-subplot(2,2,4), plot(stim), title('stim')
+subplot(2,2,2), plot(lif.k), title('k')
+subplot(2,2,3), plot(time,lif.I_stim), title('I_stim')
+subplot(2,2,4), plot(lif.stim), title('stim')
 show()
+
 
 # notes:
 # integrate for length of stimulus
