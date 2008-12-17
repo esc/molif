b65d286c3dd50f09b288d3c5f9ae51daa8786c3c
valentin <valentin@024aea0a-228c-ee1e-b6e1-9d36f26932e4>
2008-12-17 15:10:10 +0000

    implemented h
    git-svn-id: svn+ssh://bolero.cs.tu-berlin.de/home/valentin/svn/curiolab@7 024aea0a-228c-ee1e-b6e1-9d36f26932e4

--- software/l-nlif.py
+++ software/l-nlif.py
@@ -11,36 +11,33 @@ from scipy import *
 
 class lnlif:
 
-    t_max = 10000
+    def __init__(self):
+        # a place to store the spikes
+        self.spikes = [0]
 
-    # leak reversal potential : milli Volt
-    V_leak = -80 
-    # membrane leak conductance : siemens
-    g =  1.e-4
-    # reset voltage : milli Volt
-    V_reset = -80
-    # potential voltage : milli Volt
-    V_threshold = -54
+        # number of time slots to integrate until
+        self.t_max = 100000
 
-    # model of the stimulus as a vector : milli Ampere
-    stim = zeros(t_max) 
-    #noise = rand(t_max)
+        # leak reversal potential : milli Volt
+        self.V_leak = -80 
+        # membrane leak conductance : siemens
+        self.g =  1.e-4
+        # reset voltage : milli Volt
+        self.V_reset = -80
+        # potential voltage : milli Volt
+        self.V_threshold = -54
 
-    # spationtemporal linear kernel
-    # in this case modeled as a difference of gaussians
-    n = stats.distributions.norm
-    x = arange(-10,10,0.05)
-    pos = stats.distributions.norm_gen.pdf(n,x,loc=0)
-    neg = stats.distributions.norm_gen.pdf(n,x,loc=-4,scale=2)
-    k = ( pos - neg)
+        # model of the stimulus as a vector : milli Ampere
+        self.stim = zeros(self.t_max) 
+        #noise = rand(t_max)
 
-    # now convolve the linear filter k with the stimulus
-    # this really should be of length t_max
-    # check that the convolution does indeed return this.
-    # may need to pad out the linear kernel
-
-    i_stim = convolve(stim,k, mode='same')
-    spikes = []
+        # spationtemporal linear kernel
+        # in this case modeled as a difference of gaussians
+        n = stats.distributions.norm
+        x = arange(-10,10,0.05)
+        pos = stats.distributions.norm_gen.pdf(n,x,loc=0)
+        neg = stats.distributions.norm_gen.pdf(n,x,loc=-4,scale=2)
+        self.k = ( pos - neg)
 
     def set_rand_input(self):
         """ random input current """
@@ -50,20 +47,39 @@ class lnlif:
         """ constant input current """
         self.stim[:] = current
 
+    def set_depolarizing_h(self):
+        tmp = arange(0,self.t_max/5,0.1)
+        self.h = 1/exp(tmp)
+
+    def set_const_h(self):
+        tmp = arange(0,self.t_max/5,0.1)
+        self.h = zeros(len(tmp))
+
+    def set_hyperdepolarizing_h(self):
+        tmp = arange(0,self.t_max/5,0.1)
+        self.h = -1/exp(tmp)
+
+    def reset_spikes(self):
+        self.spikes = [0]
+
     def set_convolved_input(self):
         """ setup the input convolved with the linear filter"""
+        # now convolve the linear filter k with the stimulus
+        # this really should be of length t_max
+        # check that the convolution does indeed return this.
+        # may need to pad out the linear kernel
         self.i_stim = 0.2 + convolve(self.stim,self.k, mode='same')
 
     def integrate(self,x,t):
-        return -self.g*(x - self.V_leak) + self.i_stim[t] #+ i_hist(t)
+        return -self.g*(x - self.V_leak) + self.i_stim[t] + self.i_hist(t)
         
   
     def i_hist(self,t):
         # in the case where the temporal basis functions are not knowen
         # returning zero here brings us back to the more standard lif
         # model
-        # todo this needs to be implemented
-        return 0
+        return self.h[t-self.spikes[-1]]
+
 
     def euler(self, x_0, dt):
         """ euler method for for solving ODEs
@@ -82,20 +98,37 @@ class lnlif:
                 potential[i] = potential[i-1] + lif.integrate(potential[i-1],time[i-1]) * dt #+ noise[i]
         return (time, potential)
 
-lif = lnlif()
-lif.set_const_input(0.01);
-lif.i_stim = lif.stim
+
+lif = lnlif() # init model
+lif.set_const_input(0.01); # set constant input
+lif.i_stim = lif.stim # setup stimulus
 # lif.set_convolved_input();
-
-
+lif.set_const_h();
 time, potential = lif.euler(-80,1)
 
-print lif.spikes
+subplot(3,2,1), plot(time,potential), title('const h')
+subplot(3,2,2), plot(lif.h[1:200])
 
-subplot(2,3,1), plot(time,potential), title('output')
-subplot(2,3,2), plot(lif.k), title('k')
-subplot(2,3,3), plot(time,lif.i_stim), title('I_stim')
-subplot(2,3,4), plot(lif.stim), title('stim')
+lif.reset_spikes()
+lif.set_depolarizing_h();
+time, potential = lif.euler(-80,1)
+
+subplot(3,2,3), plot(time,potential), title('depolarizing h')
+subplot(3,2,4), plot(lif.h[1:200])
+
+
+lif.reset_spikes()
+lif.set_hyperdepolarizing_h();
+time, potential = lif.euler(-80,1)
+
+subplot(3,2,5), plot(time,potential), title('depolarizing h')
+subplot(3,2,6), plot(lif.h[1:200])
+
+print len(lif.h)
+
+#subplot(2,3,2), plot(lif.k), title('k')
+#subplot(2,3,3), plot(time,lif.i_stim), title('I_stim')
+#subplot(2,3,4), plot(lif.stim), title('stim')
 #subplot(2,3,5), plot(lif.h),
 show()
 
